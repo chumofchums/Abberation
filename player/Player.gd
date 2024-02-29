24fdd722a4d32f10f01e5c9d0c895f1d66827bb6
Mike H <mjh88overeasy@live.com>
2024-02-29 09:35:36 -0600

    modify base character controller

--- player/Player.gd
+++ player/Player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody3D
 
+@onready var visuals: Node3D = $visuals
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+const WALK_SPEED= 5.0
+const SPRINT_SPEED = 8.0
+#const JUMP_VELOCITY = 4.5
 
+var speed
+var gravity: float = 9.0
+
+func _ready() -> void:
+	speed = WALK_SPEED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,18 +19,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		
+		visuals.look_at(direction + position)
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
