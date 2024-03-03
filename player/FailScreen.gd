0b3054c186f3e0b6e34a5827936ca7d2db554c0a
Mike H <mjh88overeasy@live.com>
2024-03-03 12:09:55 -0600

    add fail animation

--- player/FailScreen.gd
+++ player/FailScreen.gd
@@ -3,6 +3,6 @@ extends Panel
 @onready var fail_cutscene: AnimationPlayer = $"../FailCutscene"
 
 func _on_gruel_temp_bar_gruel_timeout() -> void:
-	fail_cutscene.play("fail_screen")
+	fail_cutscene.play("fail_anim")
 	await fail_cutscene.animation_finished
 	
--- player/PlayerUI.gd
+++ player/PlayerUI.gd
@@ -6,5 +6,6 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gruel_temp_bar.visible = false
+	#fail_screen.visible = false
 
 
