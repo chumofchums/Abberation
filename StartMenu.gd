22fb761a9402f55239daccde48e7ad22c5a5f47b
Mike H <mjh88overeasy@live.com>
2024-03-04 22:20:22 -0600

    add menu music transition, sfx

--- StartMenu.gd
+++ StartMenu.gd
@@ -1,7 +1,14 @@
 extends CanvasLayer
 
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+@onready var menu_music: AudioStreamPlayer2D = $AudioStreamPlayer2D2
+
 
 func _on_start_button_pressed() -> void:
+	menu_music.stop()
+	anim.play("start_game")
+	await anim.animation_finished
 	get_tree().change_scene_to_file("res://world.tscn")
 
 
